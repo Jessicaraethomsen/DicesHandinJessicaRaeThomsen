817229fad66f8ea967801fb3d60a777f05932538
Jessicaraethomsen <Jessicaraethomsen@users.noreply.github.com>
2018-03-07 15:23:10 +0100

    Rethinking the logic and outputting compare

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 
-	var message = document.getElementById("resultScore"); // display the game result
+   
+    var message = document.getElementById("resultScore"); // display the game result
 	var playerscore = document.getElementById("playerscore"); // display the game result
 	var computerscore = document.getElementById("computerscore"); // display the game result
+	var mygame = document.getElementById("button");
+	var myreset = document.getElementById("reset");
 
-	
+	var totalcomputerscore = document.getElementById("OutputComputerScore"); // display the game result
+	var totalplayerscore = document.getElementById("OutputPlayerScore"); // display the game result
 	
 	var d1 = document.getElementById("die1");
 	var d2 = document.getElementById("die2");
 	var d3 = document.getElementById("die3");
 	var d4 = document.getElementById("die4");
-	
-	
+
+		var finalComputerScore = 0;
+		var finalHumanScore = 0;
+
 	//DICES & PLAYER CLASS:
 	
 	class Dice{
-		constructor(rollresult, rollresult2){ //here we are passing name of the dice
+		constructor(){ //here we are passing name of the dice
 				this.rollresult = 0;
 				this.rollresult2 = 0;
+			
 		}
 	
 	
@@ -24,14 +31,11 @@
 		//funcion to rolle the dice.
 		this.rollresult = Math.floor(Math.random() * 6) + 1;
 		this.rollresult2 = Math.floor(Math.random() * 6) + 1;
-		console.log(this.rollresult);
-		console.log(this.rollresult2);
+		//console.log(this.rollresult);
+		//console.log(this.rollresult2);
 		}
-	
-		get score(){
-			return  this.rollresult + this.rollresult2;
-		}
-	
+
+		
 		get roll1(){
 		 return this.rollresult;
 		}
@@ -40,14 +44,14 @@
 			return this.rollresult2;
 		}
 		
+		
 }
 	
 	class Player extends Dice{
-			constructor(type, rollresult, rollresult2){
-				//refrening to the super class "Rocket"
-				super(name, rollresult, rollresult2);  //super calls the constructor of the base clases
+			constructor(rollresult, rollresult2){
+				//refrening to the super class 
+				super(rollresult,rollresult2);  //super calls the constructor of the base clases
 				//additional class property
-				this.type = type;
 				
 			}
 		
@@ -55,77 +59,72 @@
 		rollDice(){
 			//keeping the functionality of the superclass
 			super.rollDice();
-			console.log('Player: '+ this.type); 	
+			//console.log('Player: '+ this.type); 	
 		}
+		
+		get score(){
+			return  this.rollresult + this.rollresult2;
+		}
+	
+		
 	}
 //END OF CLASSES
-	
-	
-//instantiating dices & Player
-var Human = new Player("Human");
-		Human.rollDice();
-		//Human.addResult();
-	    console.log(Human.score); //here is getter
-	
-var Computer = new Player("Computer");
-		Computer.rollDice();
-		//Computer.addResult();
-	    console.log(Computer.score); //here is getter
+
+  
+	function play(){
+	'use strict';
 		
+		//new objects Human & Computer and roll the dice math.random
+		var Human = new Player("Human");
+		Human.rollDice();
 	
-	//Global Variables
+		var Computer = new Player("Computer");
+		Computer.rollDice();
+		
+		//console.log(Human.score); //here is getter
+	   // console.log(Computer.score); //here is getter
 	
-	var s2 = Human.score;
-	var s1 = Computer.score;
-	var die1 = Human.roll1;
-	var die2 = Human.roll2;
-	var die3 = Computer.roll1;
-	var die4 = Computer.roll2;
+		//output the results
+		d3.innerHTML = Human.roll1;
+		d4.innerHTML = Human.roll2;
+		d1.innerHTML = Computer.roll1;
+		d2.innerHTML = Computer.roll2;
+		
+		//add the score
+		playerscore.innerHTML = 'Total: '  + Human.score;
+	    computerscore.innerHTML= 'Total: ' + Human.score;
 	
-	
-	 //testing output: feedback they worked!
-	console.log('Score: '+ s1 +' Testing ' + s2);
-	console.log('Human: '+ die1 +' Testing ' + die2);
-	console.log('Computer: '+ die3 +' Testing ' + die4);
-
-	
-	function reset(Human, Computer){
-	Computer.rollDice();
-	Human.rollDice();	
-	};
-	
-	
-	
-	function display(Human, Computer) {
-		//give user feedback to the user...  give the displays the information.
-		computerscore.innerHTML = 'Computer total: ' + s2;
-		playerscore.innerHTML = 'Player total: ' + s1;
-		d1.innerHTML = die1;
-		d2.innerHTML = die2;
-		d3.innerHTML = die3;
-		d4.innerHTML = die4;
-		}
-	
-	
-	function compare(Human, Computer){	
-		if(s2 === s1){
+		
+		//I KNOW THIS IS UGLY... the compare() refused to  work
+	    
+		if(Human.score=== Computer.score){
         message.innerHTML = "<span style='color:#4949ea'> TRY AGAIN A DRAW! </span>";
-		 } else if(s2 > s1){
+			//console.log("test1")
+
+		 } else if(Human.score < Computer.score){
 		 message.innerHTML = "<span style='color:#Ea4977'> YOU SADLY LOST </span>";
-		} else if(s2 < s1){
+		finalComputerScore++; //ADD THE SCOREUP
+			 //console.log("test2")
+		 
+		} else if(Human.score > Computer.score){
         message.innerHTML = "<span style='color:#0BB56E'> YOUR A WINNER, BABY </span>";
-				}	
+		finalHumanScore++;
+			
+			
+				}
+
+		   totalcomputerscore.innerHTML = 'Computer: ' + finalComputerScore;
+		   totalplayerscore.innerHTML = 'Human: ' + finalHumanScore;
+		
 			}
-	
-	
-	compare();
-	display();
-	
-	
 
-	
 
-	
+
+
+play(); //PLAY ON LOAD
+mygame.addEventListener('click', play); //PLAY ON CLICK
+
+
 
 	
 	
